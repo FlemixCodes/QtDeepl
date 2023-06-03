cd8c08b4d2642a2b9866f1b6a5f52dc53c9bbb15
FlemixCodes <Pavelvodocnikov@gmail.com>
2023-06-03 18:20:19 +0500

    rewrite module deepl

--- src/modules/deepl.py
+++ src/modules/deepl.py
@@ -4,7 +4,7 @@ import textwrap
 
 from install_playwright import install
 from playwright._impl._api_types import Error as PlaywrightError
-from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 from errors import DeepLError, DeepLPageError
 
 
@@ -56,56 +56,56 @@ class DeepL:
         self.timeout = timeout
         self.common_text = ""   
 
-    async def __async_generator_split(self, string: str) -> str | None:
+    def __generator_split(self, string: str) -> str | None:
         buffer = textwrap.wrap(string, width=self.max_lenght, break_long_words=False)
         for line in buffer:
             yield line
 
-    async def translate(self, string: str) -> str | None:
-        return await self.__translate(string)
+    def translate(self, string: str) -> str | None:
+        return self.__translate(string)
     
-    async def install_browser(self) -> bool:
-        async with async_playwright() as p:
-            code = await asyncio.get_event_loop().run_in_executor(None, install, p.chromium)
+    def install_browser(self) -> bool:
+        with sync_playwright() as p:
+            code = install(p.chromium)
             if code:
                 return True
             else:
                 return False
                 
-    async def check_browser_install(self) -> bool:
-        async with async_playwright() as p:
+    def check_browser_install(self) -> bool:
+        with sync_playwright() as p:
             try:
-                await self.__get_browser(p)
+                self.__get_browser(p)
             except PlaywrightError as e:
                 if "Executable doesn't exist at" in e.message:
                     return False
             else:
                 return True
 
-    async def __translate(self, string: str) -> Any:
-        async with async_playwright() as p:            
-            browser = await self.__get_browser(p)
+    def __translate(self, string: str) -> Any:
+        with sync_playwright() as p:            
+            browser = self.__get_browser(p)
 
-            page = await browser.new_page()
+            page = browser.new_page()
             page.set_default_timeout(self.timeout)
 
             excluded_resources = ["image", "media", "font", "other"]
-            await page.route(
+            page.route(
                 "**/*",
                 lambda route: route.abort() if route.request.resource_type in excluded_resources else route.continue_(),
             )
 
-            async for line in self.__async_generator_split(string=string):
+            for line in self.__generator_split(string=string):
                 url = "https://www.deepl.com/en/translator"
                 try:
-                    await page.goto(f"{url}#{self.fr_lang}/{self.to_lang}/{line}")
+                    page.goto(f"{url}#{self.fr_lang}/{self.to_lang}/{line}")
                     page.get_by_role("main")
                 except PlaywrightError as e:
                     msg = f"Maybe Time limit exceeded. ({self.timeout} ms)"
                     raise DeepLPageError(msg) from e
 
                 try:
-                    await page.wait_for_function(
+                    page.wait_for_function(
                         """
                         () => document.querySelector(
                         'd-textarea[data-testid=translator-target-input]')?.value?.length > 0
@@ -118,20 +118,20 @@ class DeepL:
                 input_textbox = page.get_by_role("region", name="Source text").locator("d-textarea")
                 output_textbox = page.get_by_role("region", name="Translation results").locator("d-textarea")
 
-                self.translated_fr_lang = str(await input_textbox.get_attribute("lang")).split("-")[0]
-                self.translated_to_lang = str(await output_textbox.get_attribute("lang")).split("-")[0]
+                self.translated_fr_lang = str(input_textbox.get_attribute("lang")).split("-")[0]
+                self.translated_to_lang = str(output_textbox.get_attribute("lang")).split("-")[0]
 
-                res = str((await output_textbox.all_inner_texts())[0])
+                res = str((output_textbox.all_inner_texts())[0])
                 res = res.replace("\n\n", "\n")
 
                 self.common_text += f"{res}\n"
 
-            await browser.close()
+            browser.close()
 
             return self.common_text
         
-    async def __get_browser(self, p: Any) -> Any:
-        return await p.chromium.launch(
+    def __get_browser(self, p: Any) -> Any:
+        return p.chromium.launch(
             headless=True,
             args=[
                 "--no-sandbox",
